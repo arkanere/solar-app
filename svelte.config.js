7f889ae19fba61043556a72512a4a5e670cf18a1
AniruddhaKanere <aniruddhakanere7@gmail.com>
2024-09-13 20:33:36 +0530

    Pre rendered /directory/sangli and all the business pages located in sangli /business/[business_name]

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
+import { cities } from './src/lib/entries.js'; // Include .js extension
 
-// const prerenderedCities = ['los-angeles', 'san-francisco', 'san-diego',
-// 	'houston', 'dallas', 'austin',
-// 	'new-york-city', 'buffalo', 'rochester'
-// ];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		// Add pre rendering here
-		// prerender: {
-		// 	entries: prerenderedCities.map(city => `/directory/${city}`),
-		// }
-
+		prerender: {
+			entries: ['/', '/business-listing', ...cities.map(city => `/directory/${city}`)]
+		}
 	}
 };
 
