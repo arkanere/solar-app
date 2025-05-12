a3b303afbe59968457eea0840dea7a65bf4e2381
arkanere <arkanere@gmail.com>
2025-05-12 12:52:09 +0530

    minor update

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,27 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { cities } from './src/lib/cities.js'; // Include .js extension
-import { businesses } from './src/lib/businesses.js'
+	import adapter from '@sveltejs/adapter-vercel';
+	import { cities } from './src/lib/cities.js'; // Include .js extension
+	import { businesses } from './src/lib/businesses.js'
 
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			entries: [ '/business-listing', '/business-form', '/campaign/nanded',
-				// ...cities.map(city => `/solar-panel-installer-directory/${city}`),
-				...businesses.map(business => `/solar-panel-installer/${business}`),
+	/** @type {import('@sveltejs/kit').Config} */
+	const config = {
+		kit: {
+			adapter: adapter(),
+			prerender: {
+			  entries: [ '/business-listing', '/business-form', '/campaign/nanded',
+				...businesses.map(business => `/solar-panel-installer/${business}`)
+			  ],
+			  handleHttpError: ({ path, referrer, message }) => {
+				// Ignore 405 errors specifically for the assessment-tool route
+				if (path === '/assessment-tool') {
+				  return;
+				}
+				
+				// Throw error for all other issues
+				throw new Error(message);
+			  }
+			}
+		  }
+	};
 
-			]
-		}
-	}
-};
-
-export default config;
+	export default config;
