55b79393c5108536fd8066c3a1596d3f12b1c097
arkanere <arkanere@gmail.com>
2025-06-17 13:10:02 +0530

    Implemented ISR in certain pages

--- svelte.config.js
+++ svelte.config.js
@@ -10,27 +10,6 @@ const config = {
 				expiration: 86400 // 24 hours
 			}
 		}),
-		prerender: {
-			entries: [
-				'/business-listing',
-				'/business-form',
-				'/campaign/nanded',
-				...businesses.map((business) => `/solar-panel-installer/${business}`)
-			],
-			handleHttpError: ({ path, referrer, message, status }) => {
-				// Ignore 405 errors for specific routes that have dynamic behavior
-				if (
-					status === 405 &&
-					(path === '/assessment-tool' || path === '/solar-panel-installer-directory'|| path === '/blogs')
-				) {
-					console.warn(`Skipping prerender for ${path} due to 405 error`);
-					return;
-				}
-
-				// Throw error for all other issues
-				throw new Error(message);
-			}
-		}
 	}
 };
 
