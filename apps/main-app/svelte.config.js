b09d72c2d0d982585a72e59a5bfe3935b2f395ea
arkanere <arkanere@gmail.com>
2025-09-20 18:39:05 +0530

    Implementing microfronted on vercel

--- apps/main-app/svelte.config.js
+++ apps/main-app/svelte.config.js
@@ -1,12 +1,13 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { withMicrofrontends } from '@vercel/microfrontends/experimental/sveltekit';
 import { cities } from './src/lib/cities.js';
 import { businesses } from './src/lib/businesses.js';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config = withMicrofrontends({
 	kit: {
 		adapter: adapter()
 	}
-};
+});
 
 export default config;
--- apps/main-app/vite.config.js
+++ apps/main-app/vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { microfrontends } from '@vercel/microfrontends/experimental/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), microfrontends()]
 });
