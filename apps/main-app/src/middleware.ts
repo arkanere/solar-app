01b181baa464525c8667d97ad357abd57213c329
arkanere <arkanere@gmail.com>
2026-01-14 12:38:30 +0530

    Updated hook.server.js in main-app, added missing dev dependencies in au-app. This should help with microfrontend deployment

--- apps/main-app/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { computeSourceRoute } from '@vercel/microfrontends/experimental/sveltekit';
-
-export const config = {
-	matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-};
-
-export async function middleware(request: Request) {
-	return computeSourceRoute(request);
-}
